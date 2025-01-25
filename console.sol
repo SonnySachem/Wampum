edfe8b17448146dd6fd10a2d888dd0029be05f66
SonnySachem <157922792+SonnySachem@users.noreply.github.com>
2025-01-25 23:15:46 +0000

    y

---
+++ console.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.25 <0.9.0;
+
+import "truffle/console.sol";
+
+contract Telephone {
+    uint public secret;
+
+        constructor(uint _secret) payable {
+                console.log("Telephone::constructor\n\tsecret: %o", _secret);
+                        secret = _secret;
+                            }
+
+                                function derivedSecret() public view returns (uint256) {
+                                        uint256 telephoneSecret = 100 * secret + 1;
+                                                console.log("The passphrase is: %o", secret);
+                                                        console.log("Leak it a hundred times, plus one: %o", telephoneSecret);
+                                                                return telephoneSecret;
+                                                                    }
+                                                                    }
+                                                                    
